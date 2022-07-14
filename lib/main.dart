2267d8eb07efddc00407f4eadde4ddc1fc0844ad
hvlopez <hectorvmlopez@gmail.com>
2022-07-14 17:11:04 -0500

    adding bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_eccomerce_bloc/logic/blocs/cart_bloc/cart_bloc.dart';
+import 'package:flutter_eccomerce_bloc/logic/blocs/observer_logs_bloc/observer_logs_bloc.dart';
 import 'package:flutter_eccomerce_bloc/logic/blocs/wishlist_bloc/wishlist_bloc.dart';
 import 'package:flutter_eccomerce_bloc/presentation/config/routing/app_router.dart';
 import 'package:flutter_eccomerce_bloc/presentation/config/styles/theme.dart';
 import 'package:flutter_eccomerce_bloc/presentation/screen/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
+  BlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+    blocObserver: SimpleBlocObserver(),
+  );
 }
 
 class MyApp extends StatelessWidget {
